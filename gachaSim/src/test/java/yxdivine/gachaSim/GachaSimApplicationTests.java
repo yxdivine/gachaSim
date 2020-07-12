97df0a9a06cdb0ff9703327d68f0b6f58c289381
Xinda Yu <xinday@student.unimelb.edu.au>
2020-07-13 01:57:11 +0800

    project scaffold of gacha sim

---
+++ gachaSim/src/test/java/yxdivine/gachaSim/GachaSimApplicationTests.java
@@ -0,0 +1,13 @@
+package yxdivine.gachaSim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachaSimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
