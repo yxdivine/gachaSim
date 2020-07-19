c6f5684e112ea2b48c6b23321cd03b84c1da9726
Xinda Yu <xinday@student.unimelb.edu.au>
2020-07-20 00:03:18 +0800

    rebuild proj

---
+++ gacha-parent/module-util/src/main/java/RandomHelper.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class RandomHelper {
+    private final Random random;
+
+    public RandomHelper() {
+        random = new Random();
+    }
+
+    public RandomHelper(int seed) {
+        random = new Random(seed);
+    }
+
+
+    public int getRandom(int range) {
+        return random.nextInt(range);
+    }
+
+    public int getRandom(int lb, int ub) {
+        if (lb >= ub) return lb;
+        int rand = random.nextInt(ub - lb);
+        return lb + rand;
+    }
+}
